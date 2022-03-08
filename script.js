31e5e22638581a070ae32da69327524ec1758fcc
JoyleneKirui <joylene.kirui@student.moringaschool.com>
2022-03-08 12:48:49 +0300

    update index.css

--- script.js
+++ script.js
@@ -1,43 +1,71 @@
-    let date = document.getElementById("day")
+    let dates = document.getElementById("day")
     let year = document.getElementById("Year")
     let month = document.getElementById("Month")
-    let btn = document.getElementById("buttn")
-    let myGender = document.getElementsByName("mygender");
-
+    
+   
+    
 function Calculate(){
-    let CC = parseInt(year.value.slice(0, 2));
-    let YY = parseInt(year.value.substring(2));
-    let MM = parseInt(month.value);
-    let DD =parseInt(day.value);
-    let d = Math.floor( ( (CC / 4) -2 * CC - 1) + ((5 * YY / 4) ) + ((26 * (MM + 1) / 10)) + DD ) % 7
+    let CC = (year.value.slice(0, 2));
+    let YY = (year.value.substring(2));
+    let MM = (month.value);
+    let DD = (dates.value);
+    let d = Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
     return d;
     
 }
 
-
-function getAkanName(){
+function validate() {
+    var genders = document.getElementsByName("gender"); // gender element declaration
+    if (month.value > 12  || month.value <= 0){
+       alert( "Please provide your month of birth! between 1 and 12" );
+    }
+    else if(dates.value.length >31|| dates.value <= 0) {
+       alert( "Please provide a valid date that you were born in!" );
+       document.myForm.day.focus() ;
+       return false;
+    }
+    else if(genders[0].checked==false && genders[1].checked==false ) {
+        alert("You must select male or female");
+        return false;
+    }   
+    else{
+      return true ;
+    }
+    
+  }
+function getGender(){
     let index = Calculate();
     let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     let maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var genders = document.getElementsByName("gender");
+    if(genders[0].checked == true){
+      var gender = "male";
+    }
+    else if(genders[1].checked == true){
+      var gender = "female";
+    }
+    else{
+      return false;
+    }
 
-    if(myGender == "Male"){
-        return maleAkanNames[index];
-    }
-    else if(myGender == "Female"){
-            return femaleAkanNames[index];
-    }    
-}
-function getGender(){
-    if(myGender[0].checked == true){
-        let mygender = "Male";
-    }
-    else if(myGender[1].checked == true){
-        let mygender = "Female";
-    }
-      
-}
   
-console.log(getAkanName())
-btn.addEventListener("click", getAkanName());
+  switch(gender){
+    case "male":
+        alert("You were born on " + days[index] + " and Your akan name is " +maleAkanNames[index]+"!"); //conditional case for gender>>>downwards
+    
+    break; 
+    case "female":
+      
+        alert("You were born on "+ days[index] + " and Your akan name is " + femaleAkanNames[index]+"!");
+      
+    
+    break;
+          
+  }
 
+}
+function findName(){
+
+    getGender();
+  }
